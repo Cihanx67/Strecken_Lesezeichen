b2a968a945c5268e3c99a8754f8dcc2ac6bb8270
Cihanx67 <6cihan7@gmail.com>
2014-09-26 09:41:50 +0200

    Map overlay change (nothing)

--- app/src/main/java/de/zonguldak/cihan/strecken_lesezeichen/MapOverlay.java
+++ app/src/main/java/de/zonguldak/cihan/strecken_lesezeichen/MapOverlay.java
@@ -1,16 +1,12 @@
 package de.zonguldak.cihan.strecken_lesezeichen;
 
-import android.graphics.Canvas;
-
-import com.google.android.gms.maps.MapView;
-
 /**
  * Created by cihan on 26.09.14.
  */
 public class MapOverlay {
+  /*
     public boolean draw(Canvas canvas, MapView mapView,
                         boolean shadow, long when) {
-  /*
         super.draw(canvas, mapView, shadow, when);
 
         //---translate the GeoPoint to screen pixels---
@@ -21,7 +17,7 @@ public class MapOverlay {
         Bitmap bmp = BitmapFactory.decodeResource(
                 getResources(), R.drawable.pink);
         canvas.drawBitmap(bmp, screenPts.x, screenPts.y-32, null);
-//  */
         return true;
     }
+//  */
 }
