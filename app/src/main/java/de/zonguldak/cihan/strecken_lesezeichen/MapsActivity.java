087e92eb4bc459ca8df916c972b9baf13cea78fe
Cihanx67 <6cihan7@gmail.com>
2014-09-30 11:15:11 +0200

    3D Buildings, Map Type + Menu + Icons, GPS Location, Marker: Düsseldorf, Köln, New York

--- app/src/main/java/de/zonguldak/cihan/strecken_lesezeichen/MapsActivity.java
+++ app/src/main/java/de/zonguldak/cihan/strecken_lesezeichen/MapsActivity.java
@@ -2,7 +2,12 @@ package de.zonguldak.cihan.strecken_lesezeichen;
 
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
@@ -12,18 +17,104 @@ import com.google.android.gms.maps.model.MarkerOptions;
 public class MapsActivity extends FragmentActivity {
 
     private GoogleMap mMap; // Might be null if Google Play services APK is not available.
+    private String formatForDisplay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
         setUpMapIfNeeded();
+
+        mMap.setMyLocationEnabled(true);
+
+/*        float zoomLevel = 17f;
+        LatLng zuhause = new LatLng(51.174318, 6.831817);
+
+        //automove camera to home
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(zuhause, zoomLevel));
+
+
+        //Marker Zuhause
+        mMap.addMarker(new MarkerOptions()
+                .position(new LatLng(51.1743318, 6.831817))
+                .title("Mein Zuhause"));
+
+*/
+        //automove germany
+        float zoomlevel = 5.7f;
+        LatLng germany = new LatLng(51.013879, 10.286731);
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(germany, zoomlevel));
+
+
+        //Marker New York + Einwohnerzahl
+        mMap.addMarker(new MarkerOptions()
+                .position(new LatLng(40.754408, -73.983969))
+                .title("New York")
+                .snippet("Einwohnerzahl: 8,405,837"));
+
+        //Marker Köln + Einwohnerzahl
+        mMap.addMarker(new MarkerOptions()
+                .position(new LatLng(50.937548, 6.955467))
+                .title("Köln")
+                .snippet("Einwohnerzahl: 1.034.175"));
+
+        //Marker Düsseldorf + Einwohnerzahl
+        mMap.addMarker(new MarkerOptions()
+                .position(new LatLng(51.228029, 6.776070))
+                .title("Düsseldorf")
+                .snippet("Einwohnerzahl: 598,686"));
+
+
+        //3D buildings!
+        mMap.setBuildingsEnabled(true);
+
+        //MapType Normal
+        mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+
+
+        formatForDisplay = "%.2f";
+        mMap.setOnMapLongClickListener(new GoogleMap.OnMapLongClickListener() {
+            @Override
+            public void onMapLongClick(LatLng LatLng) {
+
+                //move camera to click-point
+                //mMap.animateCamera(CameraUpdateFactory.newLatLng(LatLng));
+
+                Toast.makeText(getApplicationContext(), "Latitude: " + String.format(formatForDisplay, LatLng.latitude) + " Longitude: " + String.format(formatForDisplay, LatLng.longitude), Toast.LENGTH_LONG).show();
+            }
+
+        });
     }
 
     @Override
     protected void onResume() {
         super.onResume();
         setUpMapIfNeeded();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.maps, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()) {
+            case R.id.normal:
+                mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+                return true;
+            case R.id.hybrid:
+                mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+                return true;
+            case R.id.terrain:
+                mMap.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
+                return true;
+            default:
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     /**
@@ -66,6 +157,6 @@ public class MapsActivity extends FragmentActivity {
      */
 
 private void setUpMap() {
-        mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
+//        mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
     }
 }
